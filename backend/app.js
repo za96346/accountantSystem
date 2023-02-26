17593cae2e1ad89fbcf13c5ce4d85cd44721c0c1
siou <za96346@gmail.com>
2023-02-26 17:33:57 +0800

    add essential package

--- backend/app.js
+++ backend/app.js
@@ -1,41 +1,85 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+require('dotenv').config();
+const session = require('express-session');
+const jwt = require('jsonwebtoken');
+const expressJWT = require('express-jwt');
+const request = require('request');
+const usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// middle ware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use(expressJWT.expressjwt({
+//   secret: process.env["TOKEN_PASSWORD"],
+//   algorithms: ['HS256']
+// }).unless({ path: [/^\/login\//] }));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use(session({
+	secret: {},
+	resave: false, // 固定寫法
+	saveUninitialized: true, // 固定寫法
+}));
+
+// 登入
+app.post('/login', (req, res) => {
+	// const payload = {
+	//     username: req.body.username
+	// }
+	// const token = jwt.sign(payload, secretKey, { expiresIn: '60s' });
+	// res.send({
+	//     status: 200,
+	//     message: "登入成功!",
+	//     token
+	// });
+	const clientServerOptions = {
+		uri: 'https://workapp.tw/workApp/entry/login',
+		body: JSON.stringify({
+			Account: 'a00001',
+			Password: 'L223609254',
+		}),
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	};
+	request(clientServerOptions, (error, response) => {
+		const data = JSON.parse(response.body)
+		console.log(data.data);
+		jwt.verify(data.data, process.env.TOKEN_PASSWORD)
+	});
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use((req, res, next) => {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err, req, res, next) => {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
---
+++ table.sql
@@ -0,0 +1,46 @@
+
+use accountantSystem;
+
+# 紀錄
+create table log(
+	logId bigint not null unique auto_increment,
+	userId bigInt default -1,
+	userName varchar(50) default '',
+	companyId bigInt default -1,
+	companyCode varchar(50) default '',
+	permession int default 2,
+	routes varchar(100) default '',
+	ip varchar(100) default '',
+	params varchar(1000) default '',
+	msg varchar(1000) default '',
+	createTime timestamp default now(),
+	lastModify timestamp default now()
+);
+
+# 蘭新金流上傳
+# W, M, Y, D
+create table donationTransaction(
+	orderSid varchar(20) unique not null,
+	companyId bigInt not null,
+	amount int(6) default 0 not null,
+	cycle varchar(1) not null,
+	cyclePeriod varchar(4) not null,
+	authPeriod varchar(2) not null,
+	creditNumber varchar(2000) not null,
+	creditMaturity varchar(5) not null,
+	productName varchar(100) not null,
+	consumerName varchar(30),
+	consumerTel varchar(2000),
+	consumerAddr varchar(200),
+	consumerEmail varchar(100) not null,
+	consumerInvoiceTitle varchar(200),
+	consumerInvoiceNumver int(8),
+	recipientName varchar(200),
+	recipientTel varchar(20),
+	recipientAddr varchar(200),
+	recipientEmail varchar(100),
+	notifyUrl varchar(100),
+	lastUserEdit varchar(10),
+	createTime timestamp default now(),
+	lastModify timestamp default now()
+);
